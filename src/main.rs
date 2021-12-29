e12054ce857650cbabe650f82e0e6e66bbd21d2b
Arthur Soulié <arthur.soulie@tuta.io>
2021-12-29 12:40:42 +0100

    Add barebone web server
    Todo:
    * find a way to make requests to mongoDb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, web, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HelloWorld {
+    hello_world: String,
+}
+
+#[get("/")]
+async fn home() -> Result<impl Responder> 
+{
+    let obj = HelloWorld {
+        hello_world: String::from("Hello World!"),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>
+{
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
